4a25f439953fa5d514c57411e62cc7a3d4f7af8d
haoran1925 <48925200+haoran1925@users.noreply.github.com>
2019-04-30 00:06:49 +0800

    Update str_index_find.py

--- str_index_find.py
+++ str_index_find.py
@@ -1,4 +1,4 @@
-*Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
 def strStr(self, haystack, needle):
   lh,ln = len(haystack),len(needle)
